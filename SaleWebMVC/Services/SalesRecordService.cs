fc6e8aef15338bd9ff3a52775881c6649c3c143a
Marcelo Fiats <marcelo.fiats@yahoo.com.br>
2020-09-02 12:28:54 -0300

    Implementing grouping seach

---
+++ SaleWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SaleWebMVC.Data;
+using SaleWebMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleWebMVC.Services
+{
+    public class SalesRecordService
+    {
+        private readonly SaleWebMVCContext _context;
+
+        public SalesRecordService(SaleWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return  await result
+                .Include(x => x.Saller)
+                .Include(x => x.Saller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+        }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Saller)
+                .Include(x => x.Saller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Saller.Department)
+                .ToListAsync();
+        }
+    }
+}
